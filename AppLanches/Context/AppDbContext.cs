863bb18dfe398e751f2cb50ad42473ed4d737bd4
Alex Bezerra <51725824+OpsAlex@users.noreply.github.com>
2021-08-19 21:24:13 -0300

    ConexãoComSQLServer

---
+++ AppLanches/Context/AppDbContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using AppLanches.Models;
+
+namespace AppLanches.Context
+
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Lanche> Lanches { get; set; }
+
+        public DbSet<Categoria> Categorias { get; set; }
+
+    }
+}
---
+++ AppLanches/Models/Categoria.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppLanches.Models
+
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+        public string CategoriaNome { get; set; }
+        public string Descricao { get; set; }
+        public List<Lanche> Lanches { get; set; }
+
+    }
+}
