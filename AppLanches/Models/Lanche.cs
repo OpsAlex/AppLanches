cec606bc01e65b777fd956d5a1d4d36e1138b54b
Alex Bezerra <51725824+OpsAlex@users.noreply.github.com>
2021-08-23 20:35:24 -0300

    Realizando a Migration

--- AppLanches/Models/Lanche.cs
+++ AppLanches/Models/Lanche.cs
@@ -1,16 +1,25 @@
 
 
+using System.ComponentModel.DataAnnotations;
+
 namespace AppLanches.Models
 
 {
     public class Lanche
     {
+        
         public int LancheId { get; set; }
+        [Required]
+        [StringLength(100)]
         public string Nome { get; set; }
+        [StringLength(100)]
         public string  DescricaoCurta { get; set; }
+        [StringLength(250)]
         public string DescricaoDetalhada { get; set; }
         public decimal Preco { get; set; }
+        [StringLength(200)]
         public string ImagemUrl { get; set; }
+        [StringLength(200)]
         public string ImagemThumbnailUrl { get; set; }
         public bool IsLanchePreferido { get; set; }
         public bool EmEstoque { get; set; }
