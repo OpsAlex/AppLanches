ff8e09d8932520b5329db2e036ee247208f0a138
Alex Bezerra <51725824+OpsAlex@users.noreply.github.com>
2021-08-24 19:48:44 -0300

    Criando View/Controller Lanches

---
+++ AppLanches/Controllers/LancheController.cs
@@ -0,0 +1,33 @@
+using AppLanches.Models;
+using AppLanches.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppLanches.Controllers
+{
+    public class LancheController : Controller
+    {
+        private readonly ILancheRepository _lancheRepository;
+        private readonly ICategoriaRepository _categoriaRepository;
+
+        public LancheController(ILancheRepository lancheRepository, ICategoriaRepository categoriaRepository)
+        {
+            _lancheRepository = lancheRepository;
+            _categoriaRepository = categoriaRepository;
+        }
+
+        public IActionResult List()
+        {
+            ViewBag.Lanche = "Lanches";
+            ViewData["Categoria"] = "Categoria";
+
+            var lanches = _lancheRepository.Lanches;
+            return View(lanches);
+        }
+    }
+}
