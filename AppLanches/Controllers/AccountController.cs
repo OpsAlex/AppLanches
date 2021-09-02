04695a933f9f6c9cf81b32d28bf137c3200bb690
Alex Bezerra <51725824+OpsAlex@users.noreply.github.com>
2021-09-02 18:40:23 -0300

    Create Validation Login

---
+++ AppLanches/Controllers/AccountController.cs
@@ -0,0 +1,56 @@
+using AppLanches.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppLanches.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signManager)
+        {
+            _userManager = userManager;
+            _signInManager = signManager;
+        }
+
+        //implementar login, registro e logout
+        public IActionResult Login(string returnUrl)
+        {
+            return View(new LoginViewModel()
+            {
+                ReturnUrl = returnUrl
+            });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginVM)
+        {
+            if (!ModelState.IsValid)
+                return View(loginVM);
+
+            var user = await _userManager.FindByNameAsync(loginVM.UserName);
+            // Validando LOGIN
+
+            if (user != null)
+            {
+                var result = await _signInManager.PasswordSignInAsync(user, loginVM.Password, false, false);
+                if (result.Succeeded)
+                {
+                    if (string.IsNullOrEmpty(loginVM.ReturnUrl))
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                    return Redirect(loginVM.ReturnUrl);
+                }
+            }
+
+            ModelState.AddModelError("", "Usuário/Senha inválidos ou não localizados!!");
+            return View(loginVM);
+        }
+    }
+}
---
+++ AppLanches/ViewModels/LoginViewModel.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppLanches.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Usuário")]
+        public string UserName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Senha")]
+        public string Password { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
