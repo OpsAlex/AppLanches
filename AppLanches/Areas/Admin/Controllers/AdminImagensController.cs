dd2e16388bf5a96de28cfbb9b4c009907d3fe291
Alex Bezerra <51725824+OpsAlex@users.noreply.github.com>
2021-09-13 15:16:51 -0300

    UploadImagens

--- AppLanches/Areas/Admin/Controllers/AdminImagensController.cs
+++ AppLanches/Areas/Admin/Controllers/AdminImagensController.cs
@@ -1,8 +1,13 @@
 using AppLanches.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace AppLanches.Areas.Admin.Controllers
 {
@@ -21,6 +26,46 @@ namespace AppLanches.Areas.Admin.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> UploadFiles(List<IFormFile> files)
+        {
+            //Security
+            // Criando validações para melhor segurança do upload
+            if (files == null || files.Count == 0)
+            {
+                ViewData["Error"] = "Error: Arquivo(s) não selecionado(s)";
+                return View(ViewData);
+            }
+            if (files.Count > 10)
+            {
+                ViewData["Erro"] = "Error: Quantidade de arquivos excedeu o limite";
+                return View(ViewData);
+            }
+            long size = files.Sum(files => files.Length);
+
+            var fileParthsName = new List<string>();
+            
+            var filePath = Path.Combine(_hostingEnvironment.WebRootPath, _myConfig.NomePastaImagensProdutos);
+
+            foreach (var formFile in files)
+            {
+                if (formFile.FileName.Contains(".jpg")|| formFile.FileName.Contains(".gif")|| formFile.FileName.Contains(".png"))
+                {
+                    var fileNameWithPath = string.Concat(filePath, "\\", formFile.FileName);
+                    fileParthsName.Add(fileNameWithPath);
+
+                    using (var stream = new FileStream(fileNameWithPath, FileMode.Create))
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+                }
+            }
+            ViewData["Resultado"] = $"{files.Count} arquivos foram enviados ao servidor, " + $"com tamanho total de: {size} bytes";
+
+            ViewBag.Arquivos = fileParthsName;
+
+            return View(ViewData);
         }
     }
 }
